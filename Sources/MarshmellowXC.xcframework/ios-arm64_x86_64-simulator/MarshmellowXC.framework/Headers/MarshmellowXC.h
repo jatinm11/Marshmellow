6cbe64f4249b8b25ef608e012e66d3c22608f38a
Jatin Menghani <jatinmenghani300@gmail.com>
2024-02-15 19:24:03 +0530

    Naming issue fixed

---
+++ Sources/MarshmellowXC.xcframework/ios-arm64_x86_64-simulator/MarshmellowXC.framework/Headers/MarshmellowXC.h
@@ -0,0 +1,18 @@
+//
+//  MarshmellowXC.h
+//  MarshmellowXC
+//
+//  Created by Jatin Menghani on 15/02/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MarshmellowXC.
+FOUNDATION_EXPORT double MarshmellowXCVersionNumber;
+
+//! Project version string for MarshmellowXC.
+FOUNDATION_EXPORT const unsigned char MarshmellowXCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MarshmellowXC/PublicHeader.h>
+
+
